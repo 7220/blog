cf3c831271e67894f2ee9eb6c490a30324cf992a
7220 <kumasaki@naver.com>
2023-02-18 00:32:36 +0900

    web blog make start

---
+++ main.js
@@ -0,0 +1,32 @@
+const container = document.getElementById("root");
+
+const template = `
+<div>
+  <div class='navi'>
+    <p>asd</p>
+  </div>
+  <div class="des">
+    <div class='description'>
+      <div class='top-menu'>
+        <button>w</button>
+        <button>m</button>
+        <button>s</button>
+      </div>
+      <div>
+        <div>
+          <p class='des-in-title'>
+            asnkldnafafas
+          </p>
+        </div>
+        <div>
+          <p class='des-in-des>
+            aslknawsdas
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+container.innerHTML = template;
