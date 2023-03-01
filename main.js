7f9ca7610c4ed7c65a35082b6a18ca96cc448cfb
7220 <kumasaki@naver.com>
2023-03-02 00:30:01 +0900

    Page add

--- main.js
+++ main.js
@@ -4,23 +4,29 @@ const template = `
 <div>
   <div class='navi'>
     <p>asd</p>
+    <a href="#">
+      <img src="" alt="Github">Github</a>
   </div>
-  <div class="top-title">
-    <span>title</span>
-    <div class='top-menu'>
-      <button>Write</button>
-      <button>Modify</button>
-      <button>Sort</button>
+  <div class="background">
+    <div class="top-title">
+      <span>title</span>
+      <div class='top-menu'>
+        <button>Write</button>
+        <button>Modify</button>
+        <button>Sort</button>
+      </div>
     </div>
-  </div>
-  <div class='description'>
-    <div class='description-view'>
-      <image class='image'></image>
-      <p>title</p>
-      <p>des</p>
+    <div class='description'>
+      <a href='#' class='description-view'>
+        <image class='image'></image>
+        <div class="des-main">
+          <p class="des-title">title</p>
+          <p class="time-ago">date</p>
+        </div>
+      </a>
     </div>
   </div>
 </div>
 `;
 
-container.innerHTML = template;
+// container.innerHTML = template;
